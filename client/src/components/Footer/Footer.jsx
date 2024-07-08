5ced03eb207e41d59ba994e83b6e1cb0810917b6
Berkayilmz <berkayilmz01@gmail.com>
2024-07-08 20:48:04 +0300

    GADİR

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
+
   return (
-    <div>Footer</div>
+    <div>
+      
+    </div>
   )
 }
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -105,7 +105,7 @@ const Home = () => {
         </section>
 
       </Helmet>
-
+      
     </>
   )
 }
