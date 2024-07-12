85ba9c99bbc8d65a4523992cb180cad2b9a91aa5
Berkayilmz <berkayilmz01@gmail.com>
2024-07-12 21:08:31 +0300

    f

--- client/src/components/UI/Clock.jsx
+++ client/src/components/UI/Clock.jsx
@@ -3,15 +3,7 @@ import React from 'react'
 const Clock = () => {
   return (
     <>
-        <div className="clock_wrapper d-flex align-items-center gap-5">
-            <div className="clock_data d-flex align-items-center gap-5">
-                <div className='text-center'>
-                    <h1 className='text-white fs-3'>10 </h1>
-                    <h5 className='text-white fs-6'>Gün </h5>
-                </div>
-                <span className='text-white fs-3'>:</span>
-            </div>
-        </div>
+       
     </>
   )
 }
